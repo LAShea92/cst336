e1939dcc4a2421f083ea78f654e0e36346fef2b7
Lonnie Shea <lshea@csumb.edu>
2018-11-14 06:46:26 +0000

    Adding lab 4- Shopping cart

---
+++ labs/Slider/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Image Carousel</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <body>
+        <br /> <br />
+        <!-- HTML form goes here! -->
+        <br /><br />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    </body>
+</html>
